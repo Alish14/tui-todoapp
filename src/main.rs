110bb7fab257d0efe809b32cd743e7713efdba33
Alireza <alish14.mod@gmail.com>
2024-04-26 04:28:27 +0330

    select item and add some text to list

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{ thread, time::Duration};
+use std::{ thread, time::Duration,time::Instant};
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
@@ -6,16 +6,59 @@ use crossterm::{
 };
 use std::{error::Error, io};
 use tui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{self, Constraint, Direction, Layout,Alignment},
-    style::{Color, Modifier, Style},
-    text::{Span, Spans},
-    widgets::{Block, Borders, Tabs,Paragraph},
-    Frame, Terminal,text::Text
+    backend::{Backend, CrosstermBackend}, layout::{self, Alignment, Constraint, Direction, Layout}, style::{Color, Modifier, Style}, symbols::line, text::{Span, Spans, Text}, widgets::{Block, Borders, List, ListItem, Paragraph, Tabs,ListState}, Frame, Terminal
 };
+
+struct StatefulList<T> {
+    state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
 struct App<'a> {
     pub titles: Vec<&'a str>,
     pub index: usize,
+    items: StatefulList<(&'a str, usize)>
 }
 
 impl<'a> App<'a> {
@@ -23,6 +66,33 @@ impl<'a> App<'a> {
         App {
             titles: vec!["day1", "day2", "day3", "day4","day5","day6","day7"],
             index: 0,
+            items: StatefulList::with_items(vec![
+                ("Item0", 1),
+                ("Item1", 2),
+                ("Item2", 1),
+                ("Item3", 3),
+                ("Item4", 1),
+                ("Item5", 4),
+                ("Item6", 1),
+                ("Item7", 3),
+                ("Item8", 1),
+                ("Item9", 6),
+                ("Item10", 1),
+                ("Item11", 3),
+                ("Item12", 1),
+                ("Item13", 2),
+                ("Item14", 1),
+                ("Item15", 1),
+                ("Item16", 4),
+                ("Item17", 1),
+                ("Item18", 5),
+                ("Item19", 4),
+                ("Item20", 1),
+                ("Item21", 2),
+                ("Item22", 1),
+                ("Item23", 3),
+                ("Item24", 1),
+            ]),
         }
     }
 
@@ -45,10 +115,11 @@ fn main() -> Result<(), io::Error> {
     let mut stdout = io::stdout();
     execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
     let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
+    let mut terminal: Terminal<CrosstermBackend<io::Stdout>> = Terminal::new(backend)?;
 
     let app = App::new();
-    let res = run_app(&mut terminal, app);
+    let tick_rate = Duration::from_millis(250);
+    let res = run_app(&mut terminal, app,tick_rate);
 
     // thread::sleep(Duration::from_millis(10000));
 
@@ -67,46 +138,61 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-    loop {
-        terminal.draw(|f| ui(f, &app))?;
 
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => return Ok(()),
-                KeyCode::Right => app.next(),
-                KeyCode::Left => app.previous(),
-                _ => {}
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    mut app: App,
+    tick_rate: Duration,
+) -> io::Result<()> {
+    let mut last_tick = Instant::now();
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Char('s') => app.items.unselect(),
+                    KeyCode::Down => app.items.next(),
+                    KeyCode::Right => app.next(),
+                    KeyCode::Left => app.previous(),
+                    KeyCode::Up => app.items.previous(),
+                    _ => {}
+                }
             }
         }
     }
 }
-
-fn ui<B: Backend>(f: &mut Frame<B>, app: &App){
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let size = f.size();
     let chunks = Layout::default()
         .direction(Direction::Vertical)
         .margin(5)
-        .constraints([         
-        Constraint::Percentage(5),
-        Constraint::Percentage(100),].as_ref())
+        .constraints([Constraint::Percentage(20), Constraint::Percentage(80)].as_ref())
         .split(size);
-    let chunks_top=layout::Layout::default()
-    .direction(Direction::Vertical)
-    .constraints(
-        [
-            Constraint::Percentage(10),
-            Constraint::Percentage(80),
-            Constraint::Percentage(10),
-        ]
-        .as_ref(),
-    )
-    .split(chunks[1]);
-let chunks_down = Layout::default()
-.direction(Direction::Vertical)
-.margin(5)
-.constraints([Constraint::Length(3), Constraint::Min(1)].as_ref())
-.split(chunks[1]);
+
+    let text = vec![
+        Spans::from(Span::styled(
+            "enter q for quit and use arrow key for change tabs",
+            Style::default().fg(Color::Red),
+        )),
+    ];
+    let paragraph = Paragraph::new(text.clone()).alignment(Alignment::Center);
+    f.render_widget(paragraph, chunks[1]);
+
+    let block = Block::default()
+        .style(Style::default().bg(Color::Rgb(255, 192, 203)).fg(Color::Black));
+    f.render_widget(block, chunks[0]);
+
+    let chunks_down = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(5)
+        .constraints([Constraint::Length(3), Constraint::Min(1)].as_ref())
+        .split(chunks[1]);
+
 
     let block = Block::default().style(Style::default().bg(Color::Rgb((255), (192), (203))).fg(Color::Black));
     f.render_widget(block, size);
@@ -116,14 +202,6 @@ let chunks_down = Layout::default()
             Style::default().fg(Color::Red),
         )),
     ];
-
-   
-
-    let paragraph = Paragraph::new(text.clone())
-    .alignment(Alignment::Center);
-f.render_widget(paragraph, chunks_top[0]);
-
-
 
     let titles = app
     .titles
@@ -148,24 +226,54 @@ let tabs = Tabs::new(titles)
 f.render_widget(tabs, chunks_down[0]);
 let block = Block::default().title(app.titles[app.index]).borders(Borders::ALL);
 f.render_widget(block, chunks_down[1]);
-let chunks=layout::Layout::default()
+let chunks_list=layout::Layout::default()
     .direction(Direction::Horizontal)
     .constraints(
         [
             layout::Constraint::Percentage(50),
-            layout::Constraint::Percentage(50),
-            layout::Constraint::Percentage(50),
-        ]
+            layout::Constraint::Percentage(50),        ]
         .as_ref(),
     )
     .split(chunks_down[1]);
-// let block = Block::default().title("Tasks").borders(Borders::ALL);
-// f.render_widget(block, chunk[0]);
 
+    let items: Vec<ListItem> = app
+        .items
+        .items
+        .iter()
+        .map(|i| {
+            let mut lines = vec![Spans::from(i.0)];
+            for _ in 0..i.1 {
+                lines.push(Spans::from(Span::styled(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    Style::default().add_modifier(Modifier::ITALIC),
+                )));
+            }
+            ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
+        })
+        .collect();
+
+    let items = List::new(items)
+    .block(Block::default().borders(Borders::ALL).title("List"))
+    .highlight_style(
+        Style::default()
+            .bg(Color::LightGreen)
+            .add_modifier(Modifier::BOLD),
+    )
+    .highlight_symbol(">> ");
+f.render_stateful_widget(items, chunks_list[0], &mut app.items.state);
+
+let message = vec![
+    Spans::from(Span::styled(
+        "This is simple Tui Todo app",
+        Style::default().fg(Color::Green),
+    )),
+];
+let paragraph = Paragraph::new(message).alignment(Alignment::Center);
+f.render_widget(paragraph, chunks[0]);
 let block = Block::default().title("done").borders(Borders::ALL);
-f.render_widget(block, chunks[1]);
+f.render_widget(block, chunks_list[1]);
 let block = Block::default().title("doing").borders(Borders::ALL);
-f.render_widget(block, chunks[0]);
+f.render_widget(block, chunks_list[0]);
 
 
 
